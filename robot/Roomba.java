dfec546433325cc2df4eeb36b1252a74c178746f
MyStudyingAccount <yc6476@pleasantonusd.net>
2025-09-06 06:27:57 +0000

    modify

--- robot/Roomba.java
+++ robot/Roomba.java
@@ -4,9 +4,7 @@ import kareltherobot.*;
 
 public class Roomba implements Directions {
 
-	// Main method to make this self-contained
 	public static void main(String[] args) {
-		// LEAVE THIS ALONE!!!!!!
 		String worldName = "robot/basicRoom.wld";
 
 		Roomba cleaner = new Roomba();
@@ -15,33 +13,64 @@ public class Roomba implements Directions {
 
 	}
 
-	// declared here so it is visible in all the methods!
-	private Robot roomba;
-
-	// You will need to add many variables!!
-
+	private Robot roomba = new Robot(7, 6, North, 67);
 
 	public int cleanRoom(String worldName, int startX, int startY) {
 
-		// A new Robot should be constructed and assigned to the global (instance) variable named roomba that is declared above.
-        // Make sure it starts at startX and startY location.
-
 		World.readWorld(worldName);
 		World.setVisible(true);
-
-
-		/** This section will have all the logic that takes the Robot to every location
-		 * and cleans up all piles of beepers. Think about ways you can break this
-		 * large, complex task into smaller, easier to solve problems.
-		 */
-
-		// the line below causes a null pointer exception
-		// what is that and why are we getting it?
-		roomba.move();
-
-
-		int totalBeepers = 0; // Need to move this somewhere else.
-        // This method should return the total number of beepers cleaned up.
+		World.setDelay(1);
+		int totalBeepers = 0;
+		roomba.turnLeft();
+		roomba.turnLeft();
+		roomba.turnLeft();
+		boolean finish=false;
+		while (!finish) {
+			
+			while (roomba.frontIsClear()) {
+			
+				while (roomba.nextToABeeper()) {
+					roomba.pickBeeper();
+					totalBeepers++;
+				}
+				roomba.move();
+			}
+			if(roomba.facingEast()) {
+				roomba.turnLeft();
+				if (roomba.frontIsClear()) {
+					roomba.move();
+				}
+				else
+				{
+					break;
+				}
+				while (roomba.nextToABeeper()) {
+					roomba.pickBeeper();
+					totalBeepers++;
+				}
+				roomba.turnLeft();
+			}
+			else
+			{
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+				if (roomba.frontIsClear()) {
+					roomba.move();
+				}
+				else
+				{
+					break;
+				}
+				while (roomba.nextToABeeper()) {
+					roomba.pickBeeper();
+					totalBeepers++;
+				}
+				roomba.turnLeft();
+				roomba.turnLeft();
+				roomba.turnLeft();
+			}
+		}
 		return totalBeepers;
 	}
 }
