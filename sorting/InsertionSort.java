2cbd6a2b1c42a7d82523786f697c18349e711602
MyStudyingAccount <yc6476@pleasantonusd.net>
2026-01-26 23:12:54 +0000

    Checkpoint from VS Code for coding agent session

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,17 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        for (int i = 1; i < input.length; i++) {
+            int key = input[i];
+            int j = i - 1;
+
+            // Move elements of input[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && input[j] > key) {
+                input[j + 1] = input[j];
+                j = j - 1;
+            }
+            input[j + 1] = key;
+        }
     }
 }
